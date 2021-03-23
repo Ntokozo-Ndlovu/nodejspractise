f49b400ba88ea2eb237bfc4aa3c1d0f8d2657f43
Ntokozo <ntokozometo@gmail.com>
2021-03-23 17:36:06 +0200

    activity 3 setting up an http server using node js

---
+++ activity_3_server.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const server = http.createServer((req,res)=>{
+    res.writeHead(200,{'content-type':'text/html'});
+    res.write('Hello world');
+    res.end();
+});
+
+server.listen(3000);
